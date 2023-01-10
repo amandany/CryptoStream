a542a2cdf2780f9e97e48b15c8e4f827df734381
TRINITY <42956582+amandany@users.noreply.github.com>
2023-01-10 23:34:00 +0300

    fix

--- src/components/Home/Middle.js
+++ src/components/Home/Middle.js
@@ -4,21 +4,6 @@ import { motion } from "framer-motion";
 let advantages = ["Database", "Collect any Data", "Surveillance"];
 
 const Middle = () => {
-  const container = {
-    hidden: { opacity: 0 },
-    show: {
-      opacity: 1,
-      transition: {
-        staggerChildren: 0.5,
-      },
-    },
-  };
-
-  const item = {
-    hidden: { opacity: 0, x: -50 },
-    show: { opacity: 1, x: 0, transition: { duration: 1 } },
-  };
-
   return (
     <div className="home-middle">
       <motion.h1
